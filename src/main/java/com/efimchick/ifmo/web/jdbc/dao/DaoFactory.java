e1cc67f0653aaceb65363f33901b7c9f7445b95b
sergeypesegov <54762148+sergeypesegov@users.noreply.github.com>
2019-11-28 19:14:23 +0300

    I've done (At least I hope so)

--- src/main/java/com/efimchick/ifmo/web/jdbc/dao/DaoFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/dao/DaoFactory.java
@@ -8,9 +8,7 @@ import com.efimchick.ifmo.web.jdbc.domain.Position;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -18,19 +16,31 @@ import java.util.List;
 import java.util.Optional;
 
 public class DaoFactory {
-    private List<Employee> employees = getEmployees();
-    private List<Department> allDepartments = getDepartment();
 
     private Connection createConnection() throws SQLException{
         ConnectionSource connectionSource = ConnectionSource.instance();
         Connection connect = connectionSource.createConnection();
-            if (connect != null) {
-                System.out.println("connection is fine");
-            }
-            else {
-                System.out.println("connection was failed");
-            }
+        if (connect != null) {
+            System.out.println("connection is fine");
+        }
+        else {
+            System.out.println("connection was failed");
+        }
         return connect;
+    }
+
+    public List<Employee> mapSet(ResultSet res) throws SQLException {
+        res.beforeFirst();
+        List<Employee> emp = new ArrayList<>();
+        try {
+            while (res.next()) {
+                emp.add(mapRow(res));
+            }
+        } catch (SQLException ignore) {
+
+            return null;
+        }
+        return emp;
     }
 
     private List<Employee> getEmployees() {
@@ -102,118 +112,187 @@ public class DaoFactory {
             return null;
         }
     }
-
+    private ResultSet getResultSet(String SQL) throws SQLException {
+        ConnectionSource connectionSource = ConnectionSource.instance();
+        Connection connection = connectionSource.createConnection();
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        return statement.executeQuery(SQL);
+    }
 
     public EmployeeDao employeeDAO() {
-       return new EmployeeDao() {
-           @Override
-           public List<Employee> getByDepartment(Department department) {
-               List<Employee> empByDepartment = new ArrayList<>();
-               for (Employee emp : employees) {
-                   if (emp.getDepartmentId().equals(department.getId())) {
-                       empByDepartment.add(emp);
-                   }
-               }
-               return empByDepartment;
-           }
+        return new EmployeeDao() {
+            @Override
+            public List<Employee> getByDepartment(Department department) {
+                try {
+                    ResultSet res = getResultSet ("SELECT * FROM employee WHERE department = " + department.getId());
+                    return mapSet(res);
+                }
+                catch(SQLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-           @Override
-           public List<Employee> getByManager(Employee employee) {
-               List<Employee> empByManager = new ArrayList<>();
-               for (Employee emp : employees) {
-                   try {
-                       if (emp.getManagerId().equals(employee.getId())) {
-                           empByManager.add(emp);
-                       }
-                   }
-                   catch (NullPointerException e) {
-                       e.printStackTrace();
-                   }
-               }
-               return empByManager;
-           }
+            @Override
+            public List<Employee> getByManager(Employee employee) {
+                try {
+                    ResultSet res = getResultSet("SELECT * FROM employee WHERE manager = " + employee.getId());
+                    return mapSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-           @Override
-           public Optional<Employee> getById(BigInteger Id) {
-               Optional<Employee> employee = Optional.empty();
-               for (Employee emp : employees) {
-                   if (emp.getId().equals(Id)) {
-                       //employee.add(emp)  где-то здесь были потрачены 30 минут моей жизни,
-                       //employee.set(emp)  чтобы понять, что с Optional нужно использовать .of()
-                       employee = employee.of(emp);
-                   }
-               }
-               return employee;
-           }
+            @Override
+            public Optional<Employee> getById(BigInteger Id) {
+                try {
+                    ResultSet resultSet = getResultSet(
+                            "SELECT * FROM employee WHERE id = "
+                                    + Id.toString());
+                    if (resultSet.next()) {
+                        List<Employee> emp = mapSet(resultSet);
+                        return Optional.of(emp.get(0));
+                    }
+                    else
+                        return Optional.empty();
+                }
+                catch (SQLException e) {
+                    return Optional.empty();
+                }
+            }
 
-           @Override
-           public List<Employee> getAll() {
-               return employees;
-           }
 
-           @Override
-           public Employee save(Employee employee) {
-               try {
-                   employees.add(employee);
-                   return employee;
-               }
-               catch (Exception e){
-                   return null;
-               }
-           }
+            @Override
+            public List<Employee> getAll() {
+                try {
+                    return mapSet(getResultSet("SELECT * FROM employee"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-           @Override
-           public void delete(Employee employee) {
-               try {
-                   employees.remove(employee);
-               }
-               catch (Exception e) {
-                   e.printStackTrace();
-               }
-           }
-       };
+            @Override
+            public Employee save(Employee employee) {
+                try {
+                    getResultSet(
+                            "INSERT INTO employee VALUES ('"
+                                    + employee.getId()                       + "', '"
+                                    + employee.getFullName().getFirstName()  + "', '"
+                                    + employee.getFullName().getLastName()   + "', '"
+                                    + employee.getFullName().getMiddleName() + "', '"
+                                    + employee.getPosition()                 + "', '"
+                                    + employee.getManagerId()                + "', '"
+                                    + Date.valueOf(employee.getHired())      + "', '"
+                                    + employee.getSalary()                   + "', '"
+                                    + employee.getDepartmentId()             + "')"
+                    );
+                    return employee;
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public void delete(Employee employee) {
+                try {
+                    ConnectionSource.instance().createConnection().createStatement().execute(
+                            "DELETE FROM employee WHERE ID = " + employee.getId().toString());
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     public DepartmentDao departmentDAO() {
         //throw new UnsupportedOperationException();
         return new DepartmentDao() {
 
+            private List<Department> getDepartments(ResultSet resultSet) throws SQLException {
+                resultSet.beforeFirst();
+                List<Department> departments = new ArrayList<>();
+                try {
+                    while (resultSet.next()) {
+                        departments.add(
+                                new Department(
+                                        new BigInteger(resultSet.getString("ID")),
+                                        resultSet.getString("NAME"),
+                                        resultSet.getString("LOCATION")
+                                )
+                        );
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return departments;
+            }
+
             @Override
             public Optional<Department> getById(BigInteger Id) {
-                Optional<Department> department = Optional.empty();
-                for (Department dep : allDepartments) {
-                    if (dep.getId().equals(Id)) {
-                        department = department.of(dep);
+                try {
+                    ResultSet res = getResultSet("SELECT * FROM department WHERE id = " + Id);
+                    if (res.next() != true) {
+                        return null;
+                    } else {
+                        List<Department> dep = getDepartments(res);
+                        return Optional.of(dep.get(0));
                     }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
                 }
-                return department;
             }
 
             @Override
             public List<Department> getAll() {
-                return allDepartments;
+                try {
+                    return getDepartments(getResultSet("SELECT * FROM department"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return null;
             }
 
             @Override
             public Department save(Department department) {
-                for (int i = 0; i < allDepartments.size(); i++) {
-                    if (allDepartments.get(i).getId().equals(department.getId())) {
-                        allDepartments.remove(allDepartments.get(i));
+                try {
+                    if (getById(department.getId()).equals(Optional.empty())) {
+                        getResultSet(
+                                "INSERT INTO department VALUES ('" +
+                                        department.getId()       + "', '" +
+                                        department.getName()     + "', '" +
+                                        department.getLocation() + "')"
+                        );
+                    } else {
+                        getResultSet(
+                                "UPDATE department SET " +
+                                        "NAME = '"     + department.getName()     + "', " +
+                                        "LOCATION = '" + department.getLocation() + "' " +
+                                        "WHERE ID = '" + department.getId()       + "'"
+                        );
                     }
+                    return department;
                 }
-                allDepartments.add(department);
-                return department;
+                catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public void delete(Department department) {
                 try {
-                    allDepartments.remove(department);
+                    ConnectionSource.instance().createConnection().createStatement().execute(
+                            "DELETE FROM department WHERE ID = " + department.getId().toString());
                 }
-                catch (Exception e) {
+                catch (SQLException e) {
                     e.printStackTrace();
                 }
-
             }
         };
     }
